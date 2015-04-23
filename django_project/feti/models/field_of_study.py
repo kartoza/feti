ae7ce7e5c13f0b5f35acf3032004a4556edfe6f7
Christian Christelis <christian@kartoza.com>
2015-04-23 22:36:49 +0200

    Last models

---
+++ django_project/feti/models/field_of_study.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+"""Model class for Education and Training Quality Assurance (ETQA)."""
+__author__ = 'Christian Christelis <christian@kartoza.com>'
+__date__ = '04/2015'
+__license__ = "GPL"
+__copyright__ = 'kartoza.com'
+
+from django.contrib.gis.db import models
+
+
+class FieldOfStudy(models.Model):
+    id = models.AutoField(primary_key=True)
+    field_of_study_class = models.IntegerField()
+    field_of_study_description = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True)
+
+    def __unicode__(self):
+        return 'Field of Study Class (%s)' % self.field_of_study_class
+
+    class Meta:
+        app_label = 'feti'
---
+++ django_project/feti/models/national_certificate_vocational.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+"""Model class for WMS Resource"""
+__author__ = 'Christian Christelis <christian@kartoza.com>'
+__date__ = '04/2015'
+__license__ = "GPL"
+__copyright__ = 'kartoza.com'
+
+from django.contrib.gis.db import models
+
+
+class NationalCertificateVocational(models.Model):
+    id = models.AutoField(primary_key=True)
+    national_certificate_vocational_level = models.IntegerField()
+    national_certificate_vocational_description = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True)
+
+    def __unicode__(self):
+        return 'National Certificate Vocational Level (%s)' % (
+            self.national_certificate_vocational_level)
+
+    class Meta:
+        app_label = 'feti'
