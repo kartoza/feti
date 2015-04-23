fce140ae2d4351df96fdd6fd8247120cc1f52073
Christian Christelis <christian@kartoza.com>
2015-04-23 23:57:08 +0200

    Moving map to WC.

--- django_project/feti/static/feti/js/feti.js
+++ django_project/feti/static/feti/js/feti.js
@@ -52,7 +52,7 @@ function show_map() {
     'use strict';
     $('#navigationbar').css('height', window.innerHeight * 0.1);
     $('#map').css('height', window.innerHeight * 0.9);
-    map = L.map('map').setView([-6.2000, 106.8167], 11);
+    map = L.map('map').setView([-33.9200, 18.8600], 8);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
