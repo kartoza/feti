b8351ac9da0aba474703451d4d8f0331c0946468
Christian Christelis <christian@kartoza.com>
2015-06-03 00:37:33 +0200

    Fixing back end.

--- django_project/feti/urls.py
+++ django_project/feti/urls.py
@@ -15,10 +15,10 @@ urlpatterns = patterns(
         r'^$',
         'feti.views.landing_page.landing_page'
     ),
-    url(
-        r'^project-team/$',
-        'feti.views.project_team.project_team'
-    ),
+    # url(
+    #     r'^project-team/$',
+    #     'feti.views.project_team.project_team'
+    # ),
     # url(
     #     r'^project-team/add-campus/$',
     #     'feti.views.add_campus.add_campus'
