83fa48a6956fd1f15bc5d8f9a92a6c672d76f4ff
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2015-06-17 01:41:08 +0700

    Modify settings and deployment file: - re-expose postgresql port in docker's db (5432) to host's port 6543. Useful for development purposes. I don't know why it was changed to 5433. - change docker's dbbackups image to point to kartoza/pg-backup instead of kartoza/pg-backups. The correct one is kartoza/pg-backup. - change docker's db mounted volume from ./pg_setup_data to ./setup_data. The folder that is currently exists is ./setup_data. - uncomment docker's db mounted volume for ./pg/postgres_data:/var/lib/postgresql - modified back prod_docker.py databases settings so it uses port 5432. I don't know why it was changed to 5433, because the docker container won't work .

--- django_project/core/settings/prod_docker.py
+++ django_project/core/settings/prod_docker.py
@@ -19,7 +19,7 @@ DATABASES = {
         'USER': os.environ['DATABASE_USERNAME'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': os.environ['DATABASE_HOST'],
-        'PORT': 5433,
+        'PORT': 5432,
         'TEST_NAME': 'unittests',
     }
 }
