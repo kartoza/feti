bb477c755c745a8607c611cce4431a8d4c1e75df
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2015-06-18 12:24:09 +0700

    issue #92: Now grouping providers together. Because now different campus can be grouped on the same provider, Hover link can only happen if the course is clicked.

--- django_project/feti/views/landing_page.py
+++ django_project/feti/views/landing_page.py
@@ -18,6 +18,23 @@ from feti.models.campus import Campus
 from feti.models.course import Course
 
 
+def update_course_dict(campus_dict, campus, course):
+    if campus not in campus_dict:
+        campus_dict[campus] = [course]
+    else:
+        if course not in campus_dict[campus]:
+            campus_dict[campus].append(course)
+
+
+def update_campus_dict(provider_dict, provider, campus):
+    if provider not in provider_dict:
+        campus_dict = dict()
+        campus_dict[campus] = []
+        provider_dict[provider] = campus_dict
+    else:
+        if campus not in provider_dict[provider]:
+            provider_dict[provider][campus] = []
+
 def landing_page(request):
     """Serves the FETI landing page.
 
@@ -28,11 +45,11 @@ def landing_page(request):
     :rtype: HttpResponse
     """
     # sort the campus alphabetically
-    def campus_key(item):
-        return item[0].long_description.strip().lower()
+    def provider_key(item):
+        return item[0].primary_institution.strip().lower()
 
     search_terms = ''
-    course_dict = OrderedDict()
+    provider_dict = OrderedDict()
     errors = None
     if request.GET:
         search_terms = request.GET.get('search_terms')
@@ -41,41 +58,55 @@ def landing_page(request):
             results = SearchQuerySet().filter(
                 long_description=AutoQuery(search_terms)).models(Campus,
                                                                  Course)
+            campuses = []
 
             for result in results:
                 if result.score > 1:
                     # get model
                     model = result.model
-                    object = result.object
-                    if model == Campus and isinstance(object, Campus):
-                        campus = object
+                    # get objects
+                    object_instance = result.object
+                    # if we got campus model
+                    if model == Campus and isinstance(object_instance, Campus):
+                        campus = object_instance
                         if campus.incomplete:
                             continue
-                        course_dict[campus] = campus.courses.all()
-                    if model == Course and isinstance(object, Course):
-                        course = object
+                        if campus not in campuses:
+                            campuses.append(campus)
+                        provider = campus.provider
+                        update_campus_dict(provider_dict, provider, campus)
+                        for course in campus.courses.all():
+                            update_course_dict(
+                                provider_dict[provider], campus, course)
+                    if model == Course and isinstance(object_instance, Course):
+                        course = object_instance
                         for campus in course.campus_set.all():
-                            if campus in course_dict:
-                                if course not in course_dict[campus]:
-                                    course_dict[campus].append(course)
-                            else:
-                                course_dict[campus] = [course]
-        else:
-            campuses = Campus.objects.filter(_complete=True).order_by(
-                '_long_description')
-            for campus in campuses:
-                course_dict[campus] = campus.courses.all()
-    else:
+                            if campus.incomplete:
+                                continue
+                            if campus not in campuses:
+                                campuses.append(campus)
+                            provider = campus.provider
+                            update_campus_dict(provider_dict, provider, campus)
+                            update_course_dict(
+                                provider_dict[provider], campus, course)
+
+    if not request.GET or not search_terms:
         campuses = Campus.objects.filter(_complete=True).order_by(
             '_long_description')
         for campus in campuses:
-            course_dict[campus] = campus.courses.all()
+            if campus.incomplete:
+                continue
+            provider = campus.provider
+            update_campus_dict(provider_dict, provider, campus)
+            for course in campus.courses.all():
+                update_course_dict(provider_dict[provider], campus, course)
 
-    course_dict = OrderedDict(
-        sorted(course_dict.items(), key=campus_key))
+    provider_dict = OrderedDict(
+        sorted(provider_dict.items(), key=provider_key))
 
     context = {
-        'course_dict': course_dict,
+        'campuses': campuses,
+        'provider_dict': provider_dict,
         'search_terms': search_terms,
         'errors': errors
     }
