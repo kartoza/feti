fd34ab14e63742217ec35278c8d2b36fd30c4907
Christian Christelis <christian@kartoza.com>
2015-07-29 02:46:35 +0200

    PDF functionality added.

--- django_project/feti/static/feti/js/leaflet.control.share.js
+++ django_project/feti/static/feti/js/leaflet.control.share.js
@@ -3,6 +3,73 @@
  */
 
 /** Share control logic **/
+
+function generate_pdf(show_courses){
+
+    leafletImage(map, function(error, canvas) {
+        function add_height(doc, height, additional_height){
+            height += additional_height;
+            if (height > 180 * 3 / 2){
+                doc.addPage();
+                return 15;
+            }
+            return height;
+        }
+
+        var img = canvas.toDataURL();
+
+        var doc = new jsPDF();
+        var doc_width = 190;
+        var doc_cumulative = 10;
+
+        doc.setDrawColor(19, 70, 167);
+        doc.setFillColor(19, 70, 167);
+        doc.rect(10, doc_cumulative, doc_width, 25, 'F');
+        doc_cumulative = add_height(doc, doc_cumulative, 25 + 5);
+
+        doc.setFontSize(24);
+        doc.setTextColor(255, 255, 255);
+        doc.text(30, 25, "Further Education and Training Institute");
+
+        var map = $('#map');
+        var height = map.height();
+        var width = map.width();
+        height = doc_width * height / width;
+        width = doc_width;
+
+        doc.addImage(img, 'JPEG', 10, doc_cumulative, width, height);
+        doc_cumulative = add_height(doc, doc_cumulative, height + 5);
+
+        doc.setDrawColor(19, 70, 167);
+        doc.setFillColor(19, 70, 167);
+        doc.rect(10, doc_cumulative, doc_width, 25, 'F');
+        doc_cumulative = add_height(doc, doc_cumulative, 25 + 5);
+
+        doc_cumulative = add_height(doc, doc_cumulative, 10);
+        $('#side_panel').children(":first").children(":first").children().each(function(count, campus){
+            doc.setDrawColor(19, 70, 167);
+            doc.setFillColor(19, 70, 167);
+            doc.rect(10, doc_cumulative - 8, doc_width, 14, 'F');
+            doc.setTextColor(255, 255, 255);
+            doc.setFontSize(14);
+            doc.text(15, doc_cumulative, $(campus).find(".panel-title").text().trim());
+            doc_cumulative = add_height(doc, doc_cumulative, 15);
+            if (show_courses){
+                $(campus).find(".course-list-item").each(function(count2, course){
+                    doc.setFontSize(8);
+                    doc.setTextColor(0,0,0);
+                    doc.text(15, doc_cumulative, $(course).children(":first").text().trim());
+                    doc_cumulative = add_height(doc, doc_cumulative, 8);
+                });
+                doc_cumulative = add_height(doc, doc_cumulative, 10);
+            }
+        });
+
+        doc.save('Further Education and Training Institute.pdf');
+    });
+}
+
+
 L.Control.Share = L.Control.extend({
     options: {
         position: 'topright'
@@ -17,6 +84,7 @@ L.Control.Share = L.Control.extend({
         $("#share-control-button", container).on('click', this.shareButtonClick);
         $("#share-control-popup-close-button", container).on('click', this.closePopupClick);
         $("#share-control-pdf-button", container).on('click', this.downloadPDF);
+        $("#share-control-full-pdf-button", container).on('click', this.downloadFullPDF);
 
         // set clipboard handler
         var client = new ZeroClipboard($("#share-link-copy-button", container));
@@ -70,28 +138,10 @@ L.Control.Share = L.Control.extend({
         $("#share-control-action").css('display', 'none');
     },
     downloadPDF: function(e){
-        leafletImage(map, function(error, canvas) {
-            var img = canvas.toDataURL();
-            var doc = new jsPDF();
-            var map = $('#map');
-            var height = map.height();
-            var width = map.width();
-            height = 180*height/width;
-            width = 180;
-            doc.fromHTML(
-                $('#navbar-header').get(0),
-                0,
-                0,
-                {
-                 'width': 180 // max width of content on PDF
-                }
-                );
-            doc.setFontSize(24);
-
-            doc.text(35, 25, "FETI");
-            doc.addImage(img, 'JPEG', 15, 40, width, height);
-            doc.save('Further Education and Training Institute.pdf');
-        });
+        generate_pdf(false);
+    },
+    downloadFullPDF: function(e){
+        generate_pdf(false);
     }
 });
 
