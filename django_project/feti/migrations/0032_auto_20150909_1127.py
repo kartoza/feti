b648f1b3077b7020d92f6265732d6cb24c9cfda5
root <=>
2015-09-09 13:10:36 +0200

    Fixing up models.

---
+++ django_project/feti/migrations/0032_auto_20150909_1127.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('feti', '0031_auto_20150806_1558'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='campus',
+            options={'ordering': ['campus'], 'verbose_name': 'Provider'},
+        ),
+        migrations.AlterModelOptions(
+            name='provider',
+            options={'ordering': ['primary_institution'], 'verbose_name': 'Primary institution'},
+        ),
+        migrations.AlterField(
+            model_name='campus',
+            name='campus',
+            field=models.CharField(max_length=150, null=True, verbose_name=b'Provider', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='provider',
+            name='primary_institution',
+            field=models.CharField(max_length=255, null=True, verbose_name=b'Primary institution', blank=True),
+        ),
+    ]
