1519b8cf13ce3f603b0a23c77612f4fedb23f77d
dimasciput <dimas.ciputra@gmail.com>
2016-08-08 18:24:52 +0700

    Update feti views

--- django_project/feti/static/feti/js/feti-backbone/feti-views.js
+++ django_project/feti/static/feti/js/feti-backbone/feti-views.js
@@ -1,15 +1,8 @@
-/**
- * Created by meomancer on 08/08/16.
- */
 var MapView = Backbone.View.extend({
-    el: 'map',
-    initialize: function () {
-        this.render();
-        this.listenTo(dispatcher, 'map:add_layer', this.add_layer);
-        this.listenTo(dispatcher, 'map:remove_layer', this.remove_layer);
-    },
+    template: _.template($('#map-template').html()),
     render: function () {
-        this.map = L.map('feti-map').setView([-29, 20], 6);
+        this.$el.html(this.template());
+        this.map = L.map(this.$('#feti-map')[0]).setView([-29, 20], 6);
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
@@ -19,8 +12,13 @@ var MapView = Backbone.View.extend({
     },
     remove_layer: function (layer) {
         this.map.removeLayer(layer);
-    },
+    }
 });
+
+var mapView = new MapView();
+$('#map-container').append(mapView.el);
+mapView.render();
+
 var CourseView = Backbone.View.extend({
     tagName: 'li',
     template: _.template('<%- _long_description %>'),
@@ -35,10 +33,6 @@ var CourseView = Backbone.View.extend({
         this.$el.empty();
         this.$el.html(this.template(this.model.attributes));
         $(this.container).append(this.$el);
-    },
-    initialize: function (self, container) {
-        this.container = container;
-        this.render();
     }
 });
 
@@ -70,8 +64,5 @@ var CampusView = Backbone.View.extend({
             }
         });
         this.model.render_marker();
-    },
-    initialize: function () {
-        this.render();
     }
 });
