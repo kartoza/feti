98afbea49e92fa71b2c72a7d2b7faa2728270c80
meomancer <meomancer@gmail.com>
2016-09-05 14:52:47 +0700

    fix travis

--- django_project/feti/views/api.py
+++ django_project/feti/views/api.py
@@ -6,9 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from feti.models.campus import Campus
-from feti.models.course import Course
 from feti.serializers.campus_serializer import CampusSerializer
-from feti.serializers.course_serializer import CourseSerializer
 
 __author__ = 'Irwan Fathurrahman <irwan@kartoza.com>'
 __date__ = '08/08/16'
