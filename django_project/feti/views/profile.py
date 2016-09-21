e284082b8fcdd2c18971391f66408aa8590d2c62
dimasciput <dimas.ciputra@gmail.com>
2016-09-21 16:20:09 +0700

    Update profile view

--- django_project/feti/views/profile.py
+++ django_project/feti/views/profile.py
@@ -1,8 +1,7 @@
 from braces.views import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
-import json
-from django.core import serializers
+from django.http import Http404
 
 __author__ = 'Dimas Ciputra <dimas@kartoza.com>'
 __date__ = '21/09/16'
@@ -12,14 +11,20 @@ class UserProfileView(LoginRequiredMixin, TemplateView):
     context_object_name = 'profile'
     template_name = 'feti/user_profile.html'
 
+    def get(self, request, *args, **kwargs):
+        current_user = request.user
+        if current_user.username != self.kwargs.get('username', None):
+            raise Http404
+        return super(UserProfileView, self).get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         """Get the context data which is passed to a template."""
 
         context = super(UserProfileView, self).get_context_data(**kwargs)
         username = self.kwargs.get('username', None)
 
-        user = User.objects.filter(username=username)
+        user = User.objects.get(username=username)
 
-        context['name'] = serializers.serialize('json', user)
+        context['user'] = user
 
         return context
