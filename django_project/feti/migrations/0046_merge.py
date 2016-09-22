442d89edcd688ebcf4c1dee96d98ed0ed3d7a230
dimasciput <dimas.ciputra@gmail.com>
2016-09-22 13:38:39 +0700

    Fix migrations file

---
+++ django_project/feti/migrations/0046_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('feti', '0043_auto_20160921_1000'),
+        ('feti', '0045_auto_20160920_1427'),
+    ]
+
+    operations = [
+    ]
--- django_project/feti/migrations/0047_profile.py
+++ django_project/feti/migrations/0047_profile.py
@@ -2,25 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from django.conf import settings
 import django.contrib.gis.db.models.fields
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('feti', '0043_auto_20160921_1000'),
+        ('feti', '0046_merge'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Profile',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
-                ('bio', models.TextField(max_length=500, blank=True)),
-                ('location', django.contrib.gis.db.models.fields.PointField(srid=4326, null=True, blank=True)),
-                ('birth_date', models.DateField(null=True, blank=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('bio', models.TextField(blank=True, max_length=500)),
+                ('location', django.contrib.gis.db.models.fields.PointField(blank=True, null=True, srid=4326)),
+                ('birth_date', models.DateField(blank=True, null=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
