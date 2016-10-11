4ea681fc8a56cb7f125799f9d59b48848941bff4
dimasciput <dimas.ciputra@gmail.com>
2016-10-11 15:08:41 +0700

    Add location in campus_scraping

--- django_project/feti/management/commands/scraping_campus.py
+++ django_project/feti/management/commands/scraping_campus.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
+from django.contrib.gis.geos import GEOSGeometry
 
 from feti.models.address import Address
 from feti.models.campus import Campus
 from feti.models.provider import Provider
-from feti.utils import get_soup
+from feti.utils import get_soup, beautify
 
 __author__ = 'Irwan Fathurrahman <irwan@kartoza.com>'
 __date__ = '15/09/16'
@@ -33,6 +34,9 @@ def create_campus(data, provider):
         campus = Campus()
     campus.campus = data['campus']
     campus.provider = provider
+    if 'location' in data:
+        campus.location = GEOSGeometry('POINT(%s %s)' %
+                                       (data['location']['lng'], data['location']['lat']))
     campus.save()
     return campus
 
@@ -51,6 +55,105 @@ def create_address(data, campus):
     return address
 
 
+def scrape_campus(div):
+    provider = {}
+    campus = {}
+    address = {}
+
+    # parsing information
+    name = div.a.string
+    print("found : %s ----------------------------------------" % name)
+    provider['primary_institution'] = name.split('-')[0]
+    campus['campus'] = name.split('-')[1] if name.split('-')[1] else ""
+
+    # address
+    html = get_soup(div.a.get('href'))
+    html_address = html.find("div", {"class": "InstitutionAddress"})
+    if html_address:
+        website = html_address.a.string if html_address.a and "@" not in html_address.a.string else ""
+        provider['website'] = website or 'N/A'
+
+        content = str(html_address)
+        # get telephone
+        telephone = content.split("Telephone")
+        if len(telephone) > 1:
+            telephone = telephone[1].replace(":", "").strip().split("<")[0]
+            address['phone'] = telephone
+
+        # get street address
+        content = html_address.get_text()
+        street = content.split("Street Address")
+        if len(street) > 1:
+            street = street[1].split("Postal Address")[0].strip()
+            street = street.split("\r\n")
+            try:
+                postal_code = int(street[len(street) - 1])
+            except ValueError:
+                postal_code = None
+            # get postal code
+            if postal_code:
+                address['postal_code'] = postal_code
+                del street[-1]
+            # get town
+            if len(street) > 1:
+                address['town'] = street[len(street) - 1]
+                del street[-1]
+            address['address_line_1'] = ", ".join(street)
+
+        # get location
+        js_scripts = html.findAll("script", {"type": "text/javascript"})
+        for script in js_scripts:
+            if script.text != "" and 'mapDiv' in script.text:
+                try:
+                    locations = {
+                                    'lat': script.text.split('\r\n')[1].split(',')[1],
+                                    'lng': script.text.split('\r\n')[1].split(',')[2]
+                                }
+                    campus['location'] = locations
+                except IndexError:
+                    print('no location')
+
+    print(("provider : %s" % provider).encode('utf-8'))
+    print(("campus : %s" % campus).encode('utf-8'))
+    print(("address : %s" % address).encode('utf-8'))
+
+    # save to database
+    prov = create_provider(provider)
+    camp = create_campus(campus, prov)
+    addr = create_address(address, camp)
+
+
+def scrape_all_campuses():
+    # ----------------------------------------------------------
+    # http://ncap.careerhelp.org.za/
+    # ----------------------------------------------------------
+    page = 1
+    print("GETTING CAMPUSS AND PROVIDE IN http://ncap.careerhelp.org.za/")
+    print("----------------------------------------------------------")
+    while True:
+        # get all of list
+        print("processing page %d" % page)
+        html = get_soup('http://ncap.careerhelp.org.za/learningproviders/search/all/page/%d' % page)
+        items = html.findAll("div", {"class": "SearchResultItem"})
+        if len(items) == 0:
+            # if no item
+            break
+        elif len(items) >= 1:
+            is_empty = True
+            # tracing each of item
+            for item in items:
+                div = item.find("div", {"class": "LinkResult"})
+                if div:
+                    is_empty = False
+                    print(div)
+                    scrape_campus(div)
+            if is_empty:
+                break
+        page += 1
+        print("----------------------------------------------------------")
+    print("----------------------------------------------------------")
+
+
 class Command(BaseCommand):
     args = '<args>'
 
@@ -64,83 +167,5 @@ class Command(BaseCommand):
                 args.pop(len(args) - 1)
             elif args[len(args) - 1].lower() == "false":
                 args.pop(len(args) - 1)
-        # ----------------------------------------------------------
-        # http://ncap.careerhelp.org.za/
-        # ----------------------------------------------------------
-        page = 1
-        print("GETTING CAMPUSS AND PROVIDE IN http://ncap.careerhelp.org.za/")
-        print("----------------------------------------------------------")
-        while True:
-            # get all of list
-            print("processing page %d" % page)
-            html = get_soup('http://ncap.careerhelp.org.za/learningproviders/search/all/page/%d' % page)
-            items = html.findAll("div", {"class": "SearchResultItem"})
-            if len(items) == 0:
-                # if no item
-                break
-            elif len(items) >= 1:
-                is_empty = True
-                # tracing each of item
-                for item in items:
-                    div = item.find("div", {"class": "LinkResult"})
-                    if div:
-                        is_empty = False
-                        # find detail for campus and provider
-                        provider = {}
-                        campus = {}
-                        address = {}
 
-                        # parsing information
-                        name = div.a.string
-                        print("found : %s ----------------------------------------" % name)
-                        provider['primary_institution'] = name.split('-')[0]
-                        campus['campus'] = name.split('-')[1] if name.split('-')[1] else ""
-
-                        # address
-                        html = get_soup(div.a.get('href'))
-                        html_address = html.find("div", {"class": "InstitutionAddress"})
-                        if html_address:
-                            website = html_address.a.string if html_address.a and "@" not in html_address.a.string else ""
-                            provider['website'] = website or 'N/A'
-
-                            content = str(html_address)
-                            # get telephone
-                            telephone = content.split("Telephone")
-                            if len(telephone) > 1:
-                                telephone = telephone[1].replace(":", "").strip().split("<")[0]
-                                address['phone'] = telephone
-
-                            # get street address
-                            content = html_address.get_text()
-                            street = content.split("Street Address")
-                            if len(street) > 1:
-                                street = street[1].split("Postal Address")[0].strip()
-                                street = street.split("\r\n")
-                                try:
-                                    postal_code = int(street[len(street) - 1])
-                                except ValueError:
-                                    postal_code = None
-                                # get postal code
-                                if postal_code:
-                                    address['postal_code'] = postal_code
-                                    del street[-1]
-                                # get town
-                                if len(street) > 1:
-                                    address['town'] = street[len(street) - 1]
-                                    del street[-1]
-                                address['address_line_1'] = ", ".join(street)
-
-                        print(("provider : %s" % provider).encode('utf-8'))
-                        print(("campus : %s" % campus).encode('utf-8'))
-                        print(("address : %s" % address).encode('utf-8'))
-
-                        # save to database
-                        prov = create_provider(provider)
-                        camp = create_campus(campus, prov)
-                        addr = create_address(address, camp)
-
-                if is_empty:
-                    break
-            page += 1
-            print("----------------------------------------------------------")
-        print("----------------------------------------------------------")
+        scrape_all_campuses()
