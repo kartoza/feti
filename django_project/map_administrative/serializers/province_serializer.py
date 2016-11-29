a3705adf370278bc59bcc5e297853a4c4e5e8fb6
Dimas <dimas.ciputra@gmail.com>
2016-11-29 14:34:13 +0700

    Fix administrative boundary

--- django_project/map_administrative/serializers/province_serializer.py
+++ django_project/map_administrative/serializers/province_serializer.py
@@ -1,24 +1,19 @@
 from rest_framework_gis.serializers import GeoFeatureModelSerializer, GeometrySerializerMethodField
 from map_administrative.models.province import Province
+from rest_framework import serializers
 
 __author__ = 'irwan'
 
 
-class ProvinceSerializer(GeoFeatureModelSerializer):
-    geometry = GeometrySerializerMethodField()
-
-    def get_geometry(self, obj):
-        return None
-
+class ProvinceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Province
-        geo_field = 'polygon_geometry'
         fields = '__all__'
 
     def to_representation(self, instance):
         res = super(ProvinceSerializer, self).to_representation(instance)
-        res['properties']['id'] = instance.id
-        res['properties']['layer'] = 'province'
-        res['properties']['title'] = instance.parent() + "," + instance.name
-        res['properties']['color'] = '#f44a52'
+        res['id'] = instance.id
+        res['layer'] = 'province'
+        res['title'] = instance.parent() + "," + instance.name
+        res['color'] = '#f44a52'
         return res
