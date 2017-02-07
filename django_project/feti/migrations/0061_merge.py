e75a8d7be3d3f31ee5b32ca34bb12dc706f023cb
Dimas <dimas.ciputra@gmail.com>
2017-02-07 14:01:20 +0700

    Fix migration conflict

---
+++ django_project/feti/migrations/0061_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('feti', '0060_auto_20170206_0757'),
+        ('feti', '0060_auto_20170206_1112'),
+    ]
+
+    operations = [
+    ]
