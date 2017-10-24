3da2cc6af41ce22f5517cbf2346d7c9ba7399791
sonlinux <alison.mukoma@yahoo.com>
2017-10-24 11:27:10 +0200

    * Added Occupation listing API.

--- django_project/feti/urls.py
+++ django_project/feti/urls.py
@@ -23,6 +23,9 @@ from feti.api_views.course import (
     CourseAPIView,
     ApiCourseIds
 )
+from feti.api_views.occupation import(
+    OccupationListApiView
+)
 from feti.api_views.subfield_of_study import (
     SubFieldOfStudyAPIView
 )
@@ -125,6 +128,10 @@ api_urls = patterns(
         NationalQualificationsSubFrameworkAPIView.as_view(),
         name="api-get-nqsf"
     ),
+    url(
+        r'^api/list_occupation/',
+        OccupationListApiView.as_view(),
+        name='api-get-occupation-list'),
 )
 
 urlpatterns = patterns(
