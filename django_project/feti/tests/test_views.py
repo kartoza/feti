d436087055ac43797745a6412e6a0564270e64d9
ann26 <anita.hapsari15@alumni.imperial.ac.uk>
2017-10-26 13:37:40 +0700

    update travis and fix test

--- django_project/feti/tests/test_views.py
+++ django_project/feti/tests/test_views.py
@@ -20,9 +20,10 @@ class TestCampusViews(TestCase):
         self.client = Client()
         self.user = UserFactory.create(**{
             'username': 'dimas',
-            'password': 'password',
             'is_staff': True
         })
+        self.user.set_password('password')
+        self.user.save()
         with patch('feti.celery.update_search_index.delay') as mock:
             self.campus = CampusFactory.create()
 
@@ -55,9 +56,10 @@ class TestProviderViews(TestCase):
         self.client = Client()
         self.user = UserFactory.create(**{
             'username': 'dimas',
-            'password': 'password',
             'is_staff': True
         })
+        self.user.set_password('password')
+        self.user.save()
         self.provider = ProviderFactory.create()
 
     def tearDown(self):
