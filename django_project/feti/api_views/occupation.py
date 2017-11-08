df345500b525b6bd5badc6aa97d092a8734f3768
Alison Mukoma <alison.mukoma@yahoo.com>
2017-11-08 13:03:40 +0200

    Added Occupation listing API. (#517)
    * * Added Occupation listing API.
    
    * * Exposing only field 'occupation' in the API
    
    * * add ordering for better view of results
    
    * * changed ordering format

---
+++ django_project/feti/api_views/occupation.py
@@ -0,0 +1,21 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from feti.models.occupation import Occupation
+from feti.serializers.occupation_serializer import OccupationListSerializer
+
+__author__ = 'Alison Mukoma <alison@kartoza.com>'
+__date__ = '24/10/17'
+__license__ = "GPL"
+__copyright__ = 'kartoza.com'
+
+
+class OccupationListApiView(APIView):
+    """
+    Api to list all available occupations.
+    """
+    def get(self, request):
+        occupations = Occupation.objects.all().order_by('occupation')
+
+        serializer = OccupationListSerializer(occupations, many=True)
+        return Response(serializer.data)
